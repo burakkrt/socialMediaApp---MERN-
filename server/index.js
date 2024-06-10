3c9e444a13efa6ab3ccbe1d350b176b62bdae3d1
Burak Kurt <krtburak@outlook.com>
2024-06-10 22:52:16 +0300

    add mongodb uri and start port

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  "mongodb+srv://krtburak:Test1234.@clustersosyal.yolrgji.mongodb.net/socialDB?retryWrites=true&w=majority&appName=ClusterSosyal";
+const PORT = process.env.PORT || 4000;
 
 // json veri göndeririken veriyi pars eder
 app.use(bodyParser.json());
@@ -11,3 +15,14 @@ app.use(bodyParser.json());
 //form urlencoded ile veri göndermek için veriyi pars eder.
 // en fazla 30mb lık veri, extended ile şifrelenmiş urller üzerinde de bodyparser kullanmak için.
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Connected database.");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
